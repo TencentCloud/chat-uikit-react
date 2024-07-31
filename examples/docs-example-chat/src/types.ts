bca8b6b6359d7e319dd6d9e80b2d2b9418a73c40
angular2 <tyrosning@gmail.com>
2024-07-31 12:13:20 +0800

    chore: update readme (#44)
    Co-authored-by: tyrosning <tyrosning@tencent.com>

--- examples/docs-example-chat/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { ChatSDK } from '@tencentcloud/chat';
-
-export interface boxProps {
-  chat?: ChatSDK;
-  language?: string;
-  customClasses?: string;
-  currentConversationID?: string;
-  currentEnv?: 'web' | 'mobile';
-}
