f549afa9950c8ef4473a7b9e8ddfc3f3f474ea7f
tyrosning <tyrosning@tencent.com>
2024-10-11 18:51:03 +0800

    release: v2.3.0

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export * from './useConversation';
 export * from './useProfile';
 export * from './useMouseHover';
 export * from './useLongPress';
+
+(window as any).tencent_cloud_im_csig_react_uikit_23F_xa = true;
